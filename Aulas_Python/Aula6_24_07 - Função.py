a37f39f052647c34392abc5b33f32f2a989e2fae
LeticiaRoth <leticiaroth1@gmail.com>
2025-07-24 08:55:15 -0300

    Anotações da aula de função

---
+++ Aulas_Python/Aula6_24_07 - Função.py
@@ -0,0 +1,34 @@
+"""
+-> Funções são blocos de código reutilizáveisl, que podem ser puxados a qualquer momento;
+-Exemplos de funções já vistas, input, print, type (ver tipo da variável)
+
+
+Como identificar:
+def nome (parametros)
+    print
+
+Podemos criar variáveis através da função.
+
+--REGRAS--
+-Nome composto usar underline;
+
+--TIPOS DE FUNÇÕES--
+Com retorno:
+def soma(a, b):
+  return a + b
+resultado = soma(5, 3)
+print(f"A soma é: {resultado}")  # Saída: A soma é: 8, retorna o valor da soma
+
+
+Sem retorno:
+def imprimir_mensagem(mensagem):
+  print(mensagem)
+imprimir_mensagem("Olá, mundo!")  # Saída: Olá, mundo!
+
+
+--DIFERENÇA PRINT E RETURN--
+Return mostra o resultador da operação;
+Print posso colocar qualquer coisa dentro
+"""
+
+
