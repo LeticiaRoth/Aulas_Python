78fd399da5b7f010819a6862dd2f2d51428b2c6b
LeticiaRoth <leticiaroth1@gmail.com>
2025-07-25 10:08:00 -0300

    Anotações aula de funções com parâmetros

---
+++ Aulas_Python/Aula7_25_07 - Função com Parâmetro.py
@@ -0,0 +1,28 @@
+"""
+Funções com parâmetro, rece um ou mais valores argumentos como entrada;
+
+EX:
+def saudacao(nome):
+    print9("Olá, + nome)
+saudacao("Maria")
+
+-> Passo o parâmetro, muito utilizada quando o valor varia,
+    sempre pode mudar, como em uma calculadora.
+
+-----------------------------------------
+EX 2:
+def imparPar(numero):
+    if numero % 2 == 0:
+        print("Número par")
+    else:
+        print("Número não é par")
+
+imparPar(20)
+imparPar(89)
+
+->Sem parâmetro dara erro, com o tipo;
+
+------------------------------------------------
+EX 3:
+
+"""
