26eb03532ca26ad55faa2f250be077f28aec1e6e
LeticiaRoth <leticiaroth1@gmail.com>
2025-07-28 10:53:25 -0300

    Exemplos da aula de args e kwargs 28/07

--- Aulas_Python/Aula8_28_07 - Args e Kwargs.py
+++ Aulas_Python/Aula8_28_07 - Args e Kwargs.py
@@ -55,6 +55,52 @@ kwargs = {
 COM KWARGS fica assim:
 minhaFuncao(nome='Leticia', idade=18, cidade='Hortolândia')
 
+-----------------------------------------------
+EXEMPLOS
+
+EX 1 - Interações
+def media(num1,num2, *args):
+    print(args)
+    print(sum(args)/len(args))
+
+media(1,5,6,7,8)
+#Peguei o tamanho da tupla, divide pela soma dela, posso fazer interações com ela
+
+
+
+
+EX 2 - Colocando o asterisco para reconhecer o args e indicar a tupla
+notas = [3,6,8,9,9.96,4.2,]
+
+def somarNotas(*args):
+    print(args)
+    print(sum(args))
+
+#São varios dados em um, então coloco o asterisco e referencia o args
+somarNotas(*notas)
+
+
+
+EX 3 - Utilizando o kwargs
+def idade(**kwargs):
+    print(kwargs)
+    for nome, idade in kwargs.items():
+        print(f"Idade de {nome} é {idade}")
+
+idade(maria=5, lucas=9, jessica=8)
+
+
+
+EX 4 - Utilizando o for e o if
+def jogadas(nome, **kwargs):
+    print(kwargs)
+    for jogada in kwargs:
+        print(kwargs[jogada])
+        if kwargs[jogada] == 10:
+            return f"{nome} ganhou!"
+    return f"{nome} perdeu!"
+
+print(jogadas('Marcelo', j1=9,j2=8,j8=10,j4=9,j5=6))
 """
 
 
